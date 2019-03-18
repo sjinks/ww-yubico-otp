2eaae3886d1aead5bdfe7e37e0d21284be88c169
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2019-03-18 19:18:56 +0200

    Register autoloader

--- plugin.php
+++ plugin.php
@@ -21,4 +21,5 @@ elseif (file_exists(ABSPATH . 'vendor/autoload.php')) {
 	require ABSPATH . 'vendor/autoload.php';
 }
 
+WildWolf\WordPress\Autoloader::register();
 WildWolf\YubicoOTP\Plugin::instance();
