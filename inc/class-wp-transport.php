13b1aac3bf8b2076fd435398f578f243f4ec1126
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-06-26 07:48:29 +0300

    fix: issues found by Psalm

--- inc/class-wp-transport.php
+++ inc/class-wp-transport.php
@@ -5,7 +5,7 @@ namespace WildWolf\WordPress\YubicoOTP;
 use WildWolf\Yubico\OTP\Transport;
 use WildWolf\Yubico\OTPTransportException;
 
-class WP_Transport extends Transport {
+final class WP_Transport extends Transport {
 	protected function sendRequest( string $endpoint ): string {
 		// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.wp_remote_get_wp_remote_get
 		$response = wp_remote_get( $endpoint );
